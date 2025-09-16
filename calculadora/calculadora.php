491d6204d4ff8d99cf3dd2298290eda0a5482fda
Luis Felipe <likfelps@gmail.com>
2025-09-16 19:12:05 -0300

    commit xamp 1

---
+++ calculadora/calculadora.php
@@ -0,0 +1,12 @@
+<?php
+for($i=0; $i<10; $i++) {
+    echo "o valor de i é $i <br>";
+}
+?>
+<br><br>
+<?php
+$nomes =  ["Livia", "Luis", "Joao", "Alison", "Jaime"];
+foreach($nomes as $nome) {
+    echo "aluno: $nome <br>";
+}
+?>
